90400fd66c76640378a2b3faec6b3a6e3bad2a9d
oleg <legik7127@gmail.com>
2022-07-08 23:43:24 +0300

    added task27

---
+++ task25/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.WriteLine("Введите первое число - основание");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число - степень");
+int y = Convert.ToInt32(Console.ReadLine());
+
+void PrintdegreeOfnumber(int x, int y)
+{
+    int result = 1;
+
+    for (int i = 1; i <= y; i++)
+    {
+        result = result * x;
+    }
+
+    Console.WriteLine($"Ответ: {x} в степени {y} = {result}");
+}
+PrintdegreeOfnumber(x, y);
---
+++ task27/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 27: 
+//Напишите программу, которая принимает на вход число 
+//и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.WriteLine(" Введите любое целое число до 10-ти символов,");
+Console.WriteLine("и программа выдаст сумму цифр в данном числе:");
+int Num = Convert.ToInt32(Console.ReadLine());
+int SumOfnumber(int Num)
+{
+    int sum = 0;
+    int m = Num;
+        for (int i = 0; i <= 10; i++)
+        {
+         sum = sum + (m % 10);
+         m = m / 10;
+        }
+
+    return (sum);
+}
+int s = SumOfnumber(Num);
+Console.WriteLine(s);
